84a24569fa719cb9646feb199ad01d68d0b302ef
Ananth Sankaralingam <79415395+AnanthSankaralingam@users.noreply.github.com>
2024-10-29 18:22:00 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ config = {
   "api_type": "openai",
   "api_base": "https://api.openai.com/v1/chat/completions",
   "api_version": "2023-07-01-preview",
-  "api_key": "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"
+  "api_key": ""
 }
